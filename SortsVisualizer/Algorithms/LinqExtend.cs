b9ba4ff22cf075da597b80a8b1b4e40990949ce5
Puppy <a@a.a>
2020-05-28 13:58:41 +0500

    add settings and shuffle picture

---
+++ SortsVisualizer/Algorithms/LinqExtend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortsVisualizer.Algorithms
+{
+    public static class LinqExtend
+    {
+        private static Random rng = new Random();
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int parts)
+        {
+            return source.Select((x, i) => new { Index = i, Value = x })
+                         .GroupBy(x => x.Index / parts)
+                         .Select(x => x.Select(v => v.Value));
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ SortsVisualizer/Algorithms/SortElement.cs
@@ -0,0 +1,30 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortsVisualizer.Algorithms
+{
+    public class SortElement : IComparable<SortElement>
+    {
+        public int Value { get; set; }
+        public List<byte> Data { get; set; }
+
+        public MagickImage GetImage()
+        {
+            return new MagickImage(Data.ToArray());
+        }
+
+        public string GetBase64()
+        {
+            return Convert.ToBase64String(Data.ToArray());
+        }
+
+        public int CompareTo([AllowNull] SortElement other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
